2778463497e83c8aec5f6d6b3a89d0198a317adb
Rubina1978 <hrubesova@gmail.com>
2025-12-21 19:42:53 +0000

    Complete audio card

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,5 +40,17 @@ playBtn.addEventListener('click' , function() {
       messageScreen.style.opacity = '1';
 }
   }
+  const backBtn = document.getElementById("back-btn");
+  backBtn.addEventListener('click' , function() {
+    sound.pause();
+    sound.currentTime = 0;
+    const card = document.querySelector('.card');
+    const messageScreen = document.getElementById('message-screen');
+    if (card && messageScreen) {
+      card.style.display = 'block';
+      messageScreen.style.opacity = '0';
+      
+    }
+  } )
 });
 });
